045cec859823367b389f3cb9ecc4e7c28ab9db42
Andrew Tulloch <ajtulloch@gmail.com>
2012-04-17 16:14:58 +0800

    Using ProjectTemplate for the RCode

---
+++ ElemStatLearn Code/munge/01-A.R
@@ -0,0 +1,10 @@
+# Example preprocessing script.
+# Load training and test data
+zip.test <- as.data.frame(read.table(file="data/zip.test.wsv", header=FALSE))
+zip.train <- as.data.frame(read.table(file="data/zip.train.wsv", header=FALSE))
+
+colnames(zip.train) <- c("Y",paste("X.",1:256,sep=""))
+zip.train.filtered <- subset(zip.train, Y == 2 | Y == 3)
+
+colnames(zip.test) <- c("Y",paste("X.",1:256,sep=""))
+zip.test.filtered <- subset(zip.test, Y == 2 | Y == 3)
